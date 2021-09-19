b8d682749a11d3887995043652ba7a1a40bc3d52
Guillaume Landré <guillaumelandre101@gmail.com>
2021-09-19 18:38:25 +0200

    add product page

--- src/components/ui/select/index.jsx
+++ src/components/ui/select/index.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import {select} from './style'
+import React from 'react'
+import { select } from './style'
 
-const Select = ({options, onChange, placeholder}) => {
-
-    return (
-        <div className={select}>
-            <select onChange={e => onChange(e.target.value)}>
-                {
-                    placeholder && <option value="" disabled selected>{placeholder}</option>
+const Select = ({ options, onChange, placeholder }) => {
+  return (
+    <div className={select}>
+      <select onChange={e => onChange(e.target.value)}>
+        {
+                    placeholder && <option value='' disabled selected>{placeholder}</option>
                 }
-                {
-                    options.map(({label, value}) => (
-                        <option value={value} key={value}>{label}</option>
+        {
+                    options.map(({ label, value }) => (
+                      <option value={value} key={value}>{label}</option>
                     ))
                 }
-            </select>
-        </div>
-    );
-};
+      </select>
+    </div>
+  )
+}
 
-export default Select;
+export default Select
--- src/views/product-grid/index.jsx
+++ src/views/product-grid/index.jsx
@@ -7,8 +7,6 @@ import { API_PRODUCTS_LIST } from 'utils/api/endpoints'
 import Container from 'components/ui/container'
 import BikeCard from 'components/bike-card'
 import BikeCardSkeleton from 'components/bike-card/skeleton'
-import Space from 'components/ui/space'
-import Skeleton from 'components/skeleton'
 import Select from 'components/ui/select'
 
 import { grid, filters } from './style'
@@ -21,16 +19,16 @@ const ProductGrid = () => {
 
   const isLoading = !(bikes && !error)
 
-    const sortOptions = [
-        {label: 'asc', value: 'asc'},
-        {label: 'desc', value: 'desc'}
-    ]
+  const sortOptions = [
+    { label: 'asc', value: 'asc' },
+    { label: 'desc', value: 'desc' }
+  ]
 
   return (
     <div>
       <Container>
         <div className={filters}>
-              <Select options={sortOptions} onChange={setSort} placeholder={t('word:sort')} />
+          <Select options={sortOptions} onChange={setSort} placeholder={t('word:sort')} />
         </div>
         <div className={grid}>
           {
